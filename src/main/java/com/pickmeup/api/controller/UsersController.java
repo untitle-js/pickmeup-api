7b56cbee2dbe4f06703390d38a7a45d5593c632c
Selvaraj Karuppusamy <Selvaraj Karuppusamy>
2016-11-05 13:09:33 -0500

    working code

--- src/main/java/com/pickmeup/api/controller/UsersController.java
+++ src/main/java/com/pickmeup/api/controller/UsersController.java
@@ -1,23 +1,33 @@
 package com.pickmeup.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.pickmeup.api.dao.UserDao;
 import com.pickmeup.api.model.Users;
+import java.util.Map;
 
-import java.util.List;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by Renz on 11/2/16.
  */
 @RestController
-@RequestMapping("/users" )
+@RequestMapping("/user" )
 public class UsersController {
-
-    @RequestMapping("/register")
-    public Users  register(){
-    	UserDao dao = new UserDao();    
-    	return dao.registerUser();
+	
+    @RequestMapping(value="/register" , method = RequestMethod.GET)
+    public Users  register(@RequestParam Map<String,String> params)
+    {
+        UserDao dao = new UserDao();
+        int userId = (int)(Math.random() * 1000);
+        params.put("userId", String.valueOf(userId));
+    	return dao.registerUser(params);
+    }
+    
+    @RequestMapping(value="/login" , method = RequestMethod.GET)
+    public Users  login(@RequestParam Map<String,String> allRequest)
+    {
+        UserDao dao = new UserDao();
+    	return dao.registerUser(allRequest);
     }
 }
--- src/main/java/com/pickmeup/api/dao/UserDao.java
+++ src/main/java/com/pickmeup/api/dao/UserDao.java
@@ -1,47 +1,44 @@
 package com.pickmeup.api.dao;
 
-import java.text.Format;
-import java.sql.Date;
-
+import com.pickmeup.api.model.Users;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import java.text.SimpleDateFormat;
 
-import com.pickmeup.api.model.Users;
+import java.sql.Date;
+import java.util.Map;
 
 public class UserDao {
 	
-	public Users registerUser()
+	public Users registerUser(Map <String,String> parm)
 	{
 		SessionFactory sessionFactory = new Configuration().configure()
 				.buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
- 
+		//SimpleDateFormat formatter = new SimpleDateFormat("dd-MMMM-yy");
 		Users user = new Users();
-		user.setFirstName("Selva");
-		user.setLastName("Raj");
-		user.setEmail("selva@mail.com");
-		user.setAddress("Hopkins");
-		user.setPhoneNum(23534262);
-		user.setUserId(12);
-		user.setPwd("abcd1234");
+		user.setFirstName(parm.get("firstName"));
+		user.setLastName(parm.get("lastName"));
+		user.setEmail(parm.get("email"));
+		user.setAddress(parm.get("address"));
+		user.setPhoneNum(Integer.parseInt(parm.get("phoneNumber")));
+		user.setUserId(Integer.parseInt(parm.get("userId")));
+		user.setPwd(parm.get("password"));
 		java.util.Date uDate = new java.util.Date();
-		Date date = new Date(uDate.getTime());
+  		Date date = new Date(uDate.getTime());
 		user.setDateOfBirth(date);
 		
 		try
-		{
-			
-		
-		session.save(user);
- 
-		session.getTransaction().commit();
+		{		
+			session.saveOrUpdate(user);	 
+			session.getTransaction().commit();
 		}
 		catch(Exception ex)
 		{
-			System.out.print(ex.getMessage());
 			session.getTransaction().rollback();
+			throw ex;
 		}
 		finally
 		{
